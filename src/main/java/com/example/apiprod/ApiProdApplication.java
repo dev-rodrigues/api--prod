3049b86e480b63c290cc48c4b081f8f6303cdf88
csantos <carlos.hrq.rodrigues@gmail.com>
2023-05-12 15:56:59 -0300

    init

---
+++ src/main/java/com/example/apiprod/ApiProdApplication.java
@@ -0,0 +1,13 @@
+package com.example.apiprod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiProdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apiprod/ApiProdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiprod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
